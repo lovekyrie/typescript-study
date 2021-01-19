64205cd97bcb24359ce7d9c1d719072a836f7ce8
lovekyrie <fengdurant@163.com>
2021-01-19 22:18:24 +0800

    增加输出

--- ts-base/src/part2.project/01.module/es6/c.ts
+++ ts-base/src/part2.project/01.module/es6/c.ts
@@ -5,3 +5,12 @@ import * as All from "./a"; //导入模块中的所有成员在All上
 import myFunction from "./a"; //不加{},默认导入
 
 console.log(a, b, c);
+
+let p: P = {
+  x: 1,
+  y: 2,
+};
+
+console.log(All);
+
+myFunction();
