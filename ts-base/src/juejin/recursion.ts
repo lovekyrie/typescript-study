66ed32ec57a7609307c94aef4681f4032bf5f5d0
duanyupeng <fengdurant@163.com>
2023-05-22 16:49:44 +0800

    add recursion2

--- ts-base/src/juejin/recursion.ts
+++ ts-base/src/juejin/recursion.ts
@@ -12,3 +12,33 @@ type DeepPromiseValueType2<T> =
     ? DeepPromiseValueType2<ValueType>
       : T
 type DeepPrmiseRes2 = DeepPromiseValueType2<Promise<Promise<Promise<number>>>>
+
+type Includes<Arr extends unknown[], FindItem> = 
+  Arr extends [infer First, ...infer Rest] 
+    ? IsEqual<First, FindItem> extends true 
+      ? true
+        : Includes<Rest, FindItem>
+      : false
+type IsEqual<A, B> = (A extends B ? true :  false) & (B extends A ? true: false) 
+type IncludesRes = Includes<[1, 2, 3, 4, 5], 4>
+type IncludesRes2 = Includes<[1, 2, 3, 4, 5], 6>
+
+type RemoveItem<Arr extends unknown[], Item, Result extends unknown[] = []> =
+  Arr extends [infer First, ...infer Rest]
+    ? IsEqual<First, Item> extends true
+      ? RemoveItem<Rest, Item, Result>
+      : RemoveItem<Rest, Item, [...Result, First]>
+    : Result
+type RemoveItemRes = RemoveItem<[1, 2, 2, 3], 2>
+
+type BuildArray<Length extends number, Ele = unknown, Arr extends unknown[] = []> =
+  Arr['length'] extends Length
+    ? Arr 
+    : BuildArray<Length, Ele, [...Arr, Ele]>
+type BuildArrayRes = BuildArray<5>
+
+type ReplaceAll<Str extends string, From extends string, To extends string> = 
+  Str extends `${infer Left}${From}${infer Right}` 
+    ? `${Left}${To}${ReplaceAll<Right, From, To>}`
+      : Str
+type ReplaceAllRes = ReplaceAll<'guang guang guang', 'guang', 'dong'>
