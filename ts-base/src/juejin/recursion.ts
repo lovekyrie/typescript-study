d088dc9b3fcb663e12b33a3776f122db41f6e539
lovekyrie <fengdurant@163.com>
2023-05-23 21:57:33 +0800

    add demo

--- ts-base/src/juejin/recursion.ts
+++ ts-base/src/juejin/recursion.ts
@@ -12,3 +12,15 @@ type DeepPromiseValueType2<T> =
     ? DeepPromiseValueType2<ValueType>
       : T
 type DeepPrmiseRes2 = DeepPromiseValueType2<Promise<Promise<Promise<number>>>>
+
+type arr1 = [1, 2, 3, 4, 5]
+
+type ReverseArr<Arr extends unknown[]> = 
+  Arr extends [infer One, infer Two, infer Three, infer Four, infer Five] 
+    ? [Five, Four, Three, Two, One] : never
+type ReverseRes = ReverseArr<arr1>
+
+type ReverseArr2<Arr extends unknown[]> = 
+  Arr extends [infer First, ...infer Rest] 
+    ? [...ReverseArr2<Rest>, First] : Arr
+type ReverseRes2 = ReverseArr2<arr1>
